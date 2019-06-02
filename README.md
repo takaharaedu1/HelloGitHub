a8978cc97c88539d85543358d87e94a9cdefbd40
iMac2015 <fp7185@gmail.com>
2019-06-02 15:12:58 +0900

    history
    READMEを更新

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # HelloGitHub
+
+PullRequest Test
---
+++ src/jp/ne/tin/hellogithub/EntryPoint.java
@@ -0,0 +1,10 @@
+package jp.ne.tin.hellogithub;
+
+public class EntryPoint {
+
+	public static void main(String[] args) {
+
+		System.out.println("Hello GutHub!");
+	}
+
+}
